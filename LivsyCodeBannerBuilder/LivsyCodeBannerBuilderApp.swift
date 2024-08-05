8611ff7ddcd9d74241f7a36ac1673979b4adb6af
Livsy90 <artmir@live.ru>
2024-08-05 15:48:29 +0400

    Initial Commit

---
+++ LivsyCodeBannerBuilder/LivsyCodeBannerBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivsyCodeBannerBuilderApp.swift
+//  LivsyCodeBannerBuilder
+//
+//  Created by Livsy on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LivsyCodeBannerBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
